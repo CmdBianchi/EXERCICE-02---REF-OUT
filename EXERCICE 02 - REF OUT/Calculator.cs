cf46b780333068ddc456e4f3843af7fc19c15bfd
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-01 11:06:22 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICE 02 - REF OUT/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCICE_02___REF_OUT {
+    //---------------------------- START --------------------------------//
+    class Calculator {
+        public static void Triple(int origin, out int result) {
+            result = origin * 3;
+        }
+    }
+    //-------------------------------- END -------------------------------//
+}
---
+++ EXERCICE 02 - REF OUT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace EXERCICE_02___REF_OUT {
+    //-----> INICIO
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);         
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> FIM
+}
